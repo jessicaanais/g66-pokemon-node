6d381f7f3edf474b864fd30048df1e1e8b34d160
YOUR FULL NAME <duarte.jezzica@gmail.com>
2017-10-26 14:27:31 -0700

    migrating the seeds

--- knexfile.js
+++ knexfile.js
@@ -2,7 +2,7 @@ module.exports = {
   development: {
       client: 'pg',
       connection: {
-        database: "companyDB",
+        database: "pokemon-node",
         host: "localhost"
       },
       migrations: {
